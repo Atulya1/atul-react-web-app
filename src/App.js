a5dc1269ad38a449c1cac571d9745f1440e62e22
Atul Kumar <atul2012atul@gmail.com>
2023-01-25 17:39:49 -0500

    first commit for a2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Atul's react application
+          This is the branch for assignment 2
         </p>
         <a
           className="App-link"
