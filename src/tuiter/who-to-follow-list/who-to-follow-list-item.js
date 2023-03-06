49193aca220aa48d6ca60a4e933173469d48764b
Atul Kumar <atulkumar@atuls-mbp.lan>
2023-03-05 22:00:16 -0500

    changes for testing

--- src/tuiter/who-to-follow-list/who-to-follow-list-item.js
+++ src/tuiter/who-to-follow-list/who-to-follow-list-item.js
@@ -1,7 +1,7 @@
 import React from "react";
 const WhoToFollowListItem = (
     {
-        who = { userName: 'NASA', handle: 'NASA', avatarIcon: 'nasa.jpeg' }
+        who = { userName: "NASA", handle: "NASA", avatarIcon: "nasa.jpeg" }
     }
 ) => {
     return(
