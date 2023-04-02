77e92324459b208731f3be8f76cdc16a39794fa7
Atul Kumar <atulkumar@atuls-mbp.lan>
2023-04-02 16:46:25 -0400

    changes in service

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
+
+console.log(TUITS_API);
 export const findTuits = async () => {
     const response = await axios.get(TUITS_API);
     console.log(`services : ${response}`)
@@ -20,5 +23,6 @@ export const createTuit = async (tuit) => {
 export const updateTuit = async (tuit) => {
     const response = await axios
         .put(`${TUITS_API}/${tuit._id}`, tuit);
+    console.log(response);
     return tuit;
 }
